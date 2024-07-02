735f831a17a44b9df7db02b0235cff1cba22da7a
Booyoun <booyoun@creder.biz>
2024-07-02 16:47:54 +0900

    add getCSSVariable

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ actionButton.addEventListener('click', () => {
 });
 
 // window.Telegram.WebApp
+
+// getCSSVariable('--tg-theme-bg-color')
+// window.getComputedStyle(document.documentElement).getPropertyValue('--tg-theme-bg-color');
+function getCSSVariable(variable) {
+    return window.getComputedStyle(document.documentElement).getPropertyValue(variable);
+}
+
+// #consoleMessage 에 텍스트 추가
+function appendConsoleMessage(message) {
+    const consoleMessage = document.getElementById('consoleMessage');
+    consoleMessage.innerHTML += message + '<br>';
+}
+
+let themeBgColor = getCSSVariable('--tg-theme-bg-color');
+let themeSecondaryBgColor = getCSSVariable('--tg-theme-secondary-bg-color');
+
+appendConsoleMessage("테스트111");
+appendConsoleMessage(themeBgColor);
+appendConsoleMessage(themeSecondaryBgColor);
